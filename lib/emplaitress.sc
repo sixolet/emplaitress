3358139c3a53f80b8cbcfa732841407b1ec3904e
Naomi Seyfer <naomi@seyfer.org>
2022-12-25 05:43:20 +0000

    slides

--- lib/emplaitress.sc
+++ lib/emplaitress.sc
@@ -32,10 +32,11 @@ Emplaitress {
 	        
             SynthDef(\plaitsADSR, {
                 |out, pitch=60.0, engine=0, harm=0.1, timbre=0.5, morph=0.5, fm_mod=0.0, timb_mod=0.0,
-	    	        morph_mod=0.0, attack=0.1, decay=0.5, sustain=0.5, release=0.5, lpg_color=0.5, mul=1.0, aux_mix=0.0, gain=1.0, pan=0.0, gate=1|
+	    	        morph_mod=0.0, attack=0.1, decay=0.5, sustain=0.5, release=0.5, lpg_color=0.5,
+					mul=1.0, aux_mix=0.0, gain=1.0, pan=0.0, gate=1, pitch_lag=0.0|
 	    	    var env = EnvGen.kr(Env.adsr(attack, decay, sustain, release), gate, doneAction: Done.none);
 	    	    var sound = MiPlaits.ar(
-	    	            pitch: pitch, 
+	    	            pitch: pitch.lag(pitch_lag), 
 	    	            engine: engine, 
 	    	            harm: harm, 
 	    	            timbre: timbre + (timb_mod*env), 
@@ -65,6 +66,27 @@ Emplaitress {
 	    	    });
 	    	    notes[voice].put(note, Synth.new(\plaitsADSR, args, target: groups[voice]));
 	    	}, "emplaitress/note_on");
+	    	OSCFunc.new({ |msg, time, addr, recvPort|
+	    	    var voice = msg[1].asInteger;
+	    	    var note = msg[2].asInteger;
+				var new_note = msg[3].asInteger;
+	    	    var args = [[
+					\pitch, \engine, \harm, \timbre, \morph, \fm_mod, 
+					\timb_mod, \morph_mod, \attack, \decay, \sustain, 
+					\release, \lpg_color, \mul, \aux_mix, \gain, \pan, \pitch_lag], 
+					msg[4..]].lace;
+				//args.postln;
+				//msg[4].asFloat.postln;
+	    	    if (notes[voice].includesKey(note), {
+					"setting".postln;
+					// notes[voice][note].set(\pitch, msg[4].asFloat);
+	    	        notes[voice][note].set(*args);
+					notes[voice].put(new_note, notes[voice][note]);
+					if(note != new_note, {
+						notes[voice].removeAt(note);
+					});
+	    	    });
+	    	}, "emplaitress/note_mod");
             OSCFunc.new({ |msg, time, addr, recvPort|
                 var voice = msg[1].asInteger;
                 var note = msg[2].asInteger;
--- lib/mod.lua
+++ lib/mod.lua
@@ -7,7 +7,7 @@ end
 
 local models = {"classic analog", "waveshaping", "fm", "formant", "harmonic", "wavetable", "chord", "speech", "swarm", "noise", "particle", "string", "modal", "kick", "snare", "hat"}
 
-local style_opts = {"perc", "adsr"}
+local style_opts = {"perc", "poly", "mono"}
 
 local scale = music.generate_scale(12, "Major", 8)
 
@@ -24,7 +24,7 @@ local function n(i, s)
 end
 
 function add_plaits(i)
-    params:add_group(n("group", i), "emplait voice "..i, 21)
+    params:add_group(n("group", i), "emplait voice "..i, 22)
     params:hide(n("group", i))
     params:add_option(n(i, "style"), "style", style_opts, 1)
     params:set_action(n(i, "style"), function(s)
@@ -36,7 +36,7 @@ function add_plaits(i)
             params:hide(n(i, "s"))
             params:hide(n(i, "r"))
             params:show(n(i, "decay"))
-        elseif s == 2 then
+        elseif s == 2 or s == 3 then
             params:hide(n(i, "trigger"))
             params:show(n(i, "gate"))
             params:show(n(i, "a"))
@@ -44,6 +44,11 @@ function add_plaits(i)
             params:show(n(i, "s"))
             params:show(n(i, "r"))
             params:hide(n(i, "decay"))
+        end
+        if s == 3 then
+            params:show(n(i, "slew"))
+        else
+            params:hide(n(i, "slew"))
         end
         _menu.rebuild_params()
     end)
@@ -76,6 +81,7 @@ function add_plaits(i)
     params:add_control(n(i, "lpg_color"), "lpg color", controlspec.new(0, 1, "lin", 0, 0.5))
     params:add_control(n(i, "gain"), "gain", controlspec.new(0, 3, "lin", 0, 1))
     params:add_control(n(i, "pan"), "pan", controlspec.new(-1, 1, "lin", 0, 0))
+    params:add_control(n(i, "slew"), "slew", controlspec.new(0, 1, "lin", 0, 0))
 
 
 
@@ -160,6 +166,10 @@ for voice=1,4,1 do (function (i)
         end
     end
 
+    function player:set_slew(s)
+        params:set(n(i, "slew"), s)
+    end
+
     function player:note_on(note, vel)
         if params:get(n(i, "style")) == 1 then
             osc.send({"localhost", 57120}, "/emplaitress/perc", {
@@ -200,14 +210,67 @@ for voice=1,4,1 do (function (i)
                 params:get(n(i, "gain")), -- post-plaits gain
                 params:get(n(i, "pan")) -- pan
             })
+        elseif params:get(n(i, "style")) == 3 then
+            if self.current_note then
+                osc.send({"localhost", 57120}, "/emplaitress/note_mod", {
+                    i - 1,
+                    self.current_note,
+                    note,
+                    music.freq_to_note_num(music.note_num_to_freq(note)), --pitch. Round trip through music lib for tuning mod support.
+                    params:get(n(i, "model")) - 1, --engine
+                    params:get(n(i, "harmonics")), --harm
+                    params:get(n(i, "timbre")), --timbre
+                    params:get(n(i, "morph")), --morph
+                    params:get(n(i, "fm_mod")), --fm_mod
+                    params:get(n(i, "timb_mod")), -- timb mod
+                    params:get(n(i, "morph_mod")), --morph mod
+                    params:get(n(i, "a")), --attack
+                    params:get(n(i, "d")), --decay
+                    params:get(n(i, "s")), --sustain
+                    params:get(n(i, "r")), --release                
+                    params:get(n(i, "lpg_color")), --lpg_color
+                    params:get(n(i, "amp"))*vel*vel, --mul
+                    params:get(n(i, "aux")), --aux_mix
+                    params:get(n(i, "gain")), -- post-plaits gain
+                    params:get(n(i, "pan")), -- pan
+                    params:get(n(i, "slew")) -- pitch_lag
+                })
+                self.current_note = note
+            else
+                osc.send({"localhost", 57120}, "/emplaitress/note_on", {
+                    i - 1,
+                    note,
+                    music.freq_to_note_num(music.note_num_to_freq(note)), --pitch. Round trip through music lib for tuning mod support.
+                    params:get(n(i, "model")) - 1, --engine
+                    params:get(n(i, "harmonics")), --harm
+                    params:get(n(i, "timbre")), --timbre
+                    params:get(n(i, "morph")), --morph
+                    params:get(n(i, "fm_mod")), --fm_mod
+                    params:get(n(i, "timb_mod")), -- timb mod
+                    params:get(n(i, "morph_mod")), --morph mod
+                    params:get(n(i, "a")), --attack
+                    params:get(n(i, "d")), --decay
+                    params:get(n(i, "s")), --sustain
+                    params:get(n(i, "r")), --release                
+                    params:get(n(i, "lpg_color")), --lpg_color
+                    params:get(n(i, "amp"))*vel*vel, --mul
+                    params:get(n(i, "aux")), --aux_mix
+                    params:get(n(i, "gain")), -- post-plaits gain
+                    params:get(n(i, "pan")), -- pan
+                    params:get(n(i, "slew")) -- pitch_lag
+                })
+                self.current_note = note
+            end
         end
     end
 
     function player:note_off(note)
         -- pass, for perc.
+        if self.current_note and self.current_note == note then
+            self.current_note = nil
+        end
         osc.send({"localhost", 57120}, "/emplaitress/note_off", {i - 1, note});
     end
-
     if note_players == nil then
         note_players = {}
     end
@@ -229,7 +292,9 @@ function pre_init()
             local s = scale_names[params:get("plaits_scale")]
             scale = music.generate_scale(params:get("plaits_root"), s, 8)
         end)
-        matrix:defer_bang("plaits_root")
+        if matrix ~= nil then
+            matrix:defer_bang("plaits_root")
+        end
         for i=1,4,1 do
             add_plaits(i)
         end
